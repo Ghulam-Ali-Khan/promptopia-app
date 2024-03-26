bb3857ceb9bd3a5c8d8a62c2aa5002a58fef121a
Ghulam Ali <gulamali@beyonderissolutions.com>
2024-03-26 14:53:19 +0500

    Some corrections

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,14 +1,12 @@
 "use client";
 
 import { useEffect, useState } from 'react';
-import { useSession } from 'next-auth/react';
 import { useRouter, useSearchParams } from 'next/navigation';
 
 import Form from '@components/Form';
 
 const UpdatePrompt = () => {
     const router = useRouter();
-    const { data: session } = useSession();
     const searchParams = useSearchParams();
     const promptId = searchParams.get('id');
 
